bbb34955c5fd18e93a84e6bb59430ef025ec90e5
Roberto James <roberto.junior.james13@gmail.com>
2024-10-28 23:05:29 -0500

    modify database connection to use cloud

--- Code/Database/database_connection.py
+++ Code/Database/database_connection.py
@@ -3,8 +3,9 @@ from mysql.connector import Error
 
 def get_db_connection():
     try:
+
         # Attempt to establish a connection to the MySQL database
-        dbConn = mysql.connector.connect(host="localhost",user="root",password="",database="student_info")
+        dbConn = mysql.connector.connect(host="sql5.freesqldatabase.com",user="sql5741398",password="6L6Cfq6Msn",database="sql5741398")
         
         # Check if the connection was successful
         if dbConn.is_connected():
@@ -14,4 +15,6 @@ def get_db_connection():
         # Print an error message if the connection failed
         print(f"Error connecting to the database: {e}")
         return None
+    
+
     
