643a6a24d9b7fdd444d60bcbcbe8c99646f441a0
Roberto James <roberto.junior.james13@gmail.com>
2024-11-05 07:44:18 -0500

    clear button now works for the login interface

--- Code/Login_Interface.py
+++ Code/Login_Interface.py
@@ -5,10 +5,11 @@ import tkinter.messagebox as messagebox
 
 #functions
 
-# Function to clear username and password fields
-def clearFields(usernameEntry, passwordEntry):
+# Function to clear username, password fields, and reset dropdown
+def clearFields(usernameEntry, passwordEntry, dropdown):
     usernameEntry.delete(0, 'end')
     passwordEntry.delete(0, 'end')
+    dropdown.set("Select a User Type")
 
 
 def validateLoginDetails(usernameEntry,passwordEntry,dropdown,root,frame):
@@ -103,7 +104,7 @@ def login_interface(root):
     bottom_border.grid(row=7, column=0, pady=(0, 15), columnspan=2)
 
     # Clear and Submit buttons
-    clearButton = tk.Button(frame,text="Clear", font=("Arial", 12),padx=20,bg="#007bff",fg="white",width=6, command= lambda:clearFields(usernameEntry,passwordEntry))
+    clearButton = tk.Button(frame,text="Clear", font=("Arial", 12),padx=20,bg="#007bff",fg="white",width=6, command= lambda:clearFields(usernameEntry,passwordEntry,dropdown))
     clearButton.grid(row=8, column=0,sticky="w")
 
     submitButton = tk.Button(frame, text="Submit",font=("Arial", 12), padx=20, bg="#007bff",fg="white", width=6, command= lambda:validateLoginDetails(usernameEntry,passwordEntry,dropdown,root,frame))
